d893726aa4ebf780036442aa16b6f430c6455167
mislavzanic <mislavzanic3@gmail.com>
2021-07-11 12:13:56 +0200

    backup

--- backup.sh
+++ backup.sh
@@ -2,7 +2,7 @@
 
 [[ -z ~/Dotfiles ]] && mkdir ~/Dotfiles
 
-[[ -z ~/Dotfiles/suckless ]] && cp -r ~/.config/suckless ~/Dotfiles/suckless
+[[ ~/Dotfiles/suckless ]] && cp -r ~/.config/suckless ~/Dotfiles/suckless
 [[ -z ~/Dotfiles/shell ]] && cp -r ~/.config/shell ~/Dotfiles/shell
 [[ -z ~/Dotfiles/zsh ]] && cp -r ~/.config/zsh ~/Dotfiles/zsh
 [[ -z ~/Dotfiles/x11 ]] && cp -r ~/.config/x11 ~/Dotfiles/x11
