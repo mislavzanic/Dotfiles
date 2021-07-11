978626ffa9145d16412d0b039fd9e81a42ceb7ea
mislavzanic <mislavzanic3@gmail.com>
2021-07-11 12:14:13 +0200

    backup

--- backup.sh
+++ backup.sh
@@ -3,9 +3,9 @@
 [[ -z ~/Dotfiles ]] && mkdir ~/Dotfiles
 
 [[ ~/Dotfiles/suckless ]] && cp -r ~/.config/suckless ~/Dotfiles/suckless
-[[ -z ~/Dotfiles/shell ]] && cp -r ~/.config/shell ~/Dotfiles/shell
-[[ -z ~/Dotfiles/zsh ]] && cp -r ~/.config/zsh ~/Dotfiles/zsh
-[[ -z ~/Dotfiles/x11 ]] && cp -r ~/.config/x11 ~/Dotfiles/x11
+[[ ~/Dotfiles/shell ]] && cp -r ~/.config/shell ~/Dotfiles/shell
+[[ ~/Dotfiles/zsh ]] && cp -r ~/.config/zsh ~/Dotfiles/zsh
+[[ ~/Dotfiles/x11 ]] && cp -r ~/.config/x11 ~/Dotfiles/x11
 
 [[ -z .git ]] && git init . && git remote add origin git@github.com:mislavzanic/Dotfiles.git
 git add .
